a8f2a218a4916c7986a6dd3d9963eedd0ceebce1
Almog <aalmogiiii@gmail.com>
2022-08-23 11:03:06 +0300

    added Phonebook interface

--- src/IPhonebook.ts
+++ src/IPhonebook.ts
@@ -1,1 +1,14 @@
-1
+import Contant from "./Contact"
+
+interface IPhonebook {
+    size:number //- gets the amount of contacts
+    add(contact:Contact):number //- add contact and returns its new id
+    addPhone(id:number, phone:string):void //- add new phone to existing contact
+    get(id:number):Contact|undefined //- get contact by id
+    get(name:string):Contact[]|undefined //- get contacts by name
+    remove(id:number):Contact|undefined //- remove contact by id and returns it
+}
+
+export default IPhonebook
+
+
